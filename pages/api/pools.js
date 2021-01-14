c9bd964c5814c5e69a0c871b48b3e1108d6d0847
Symblox Dev <symblox@protonmail.com>
2021-01-14 11:16:42 +0800

    Initial commit

--- pages/api/pools.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ pool_identifier: 'syx/vlx', liquidity_locked: 123456, apy: 214.3 }))
-  }
