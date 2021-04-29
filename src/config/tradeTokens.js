1cb519c7c35379eb81a31cd5da94b58acab08c87
Symblox Dev <symblox@protonmail.com>
2021-04-29 21:36:52 +0800

    chg: add syx3 img remove svlx.!minor

--- src/config/tradeTokens.js
+++ src/config/tradeTokens.js
@@ -3,21 +3,9 @@ const tokens = getTokens();
 
 function getTradeTokens() {
   if (process.env.REACT_APP_ENV === "production") {
-    return [
-      tokens["VLX"],
-      tokens["SVLX"],
-      tokens["SYX"],
-      tokens["USDT"],
-      tokens["ETH"],
-    ];
+    return [tokens["VLX"], tokens["SVLX"], tokens["SYX"], tokens["USDT"], tokens["ETH"]];
   } else {
-    return [
-      tokens["VLX"],
-      tokens["SVLX"],
-      tokens["SYX"],
-      tokens["USDT"],
-      tokens["ETH"],
-    ];
+    return [tokens["VLX"], tokens["SYX"], tokens["USDT"], tokens["ETH"]];
   }
 }
 export default getTradeTokens;
