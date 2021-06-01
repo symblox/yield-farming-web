960d7c7746c514a357fa4b20ad6ed1b30cb9d8eb
Symblox Dev <symblox@protonmail.com>
2021-06-01 17:01:50 +0800

    chg: update contract address.

--- src/hooks/usePoolTokenBalance.js
+++ src/hooks/usePoolTokenBalance.js
@@ -1,36 +1,26 @@
-import { useEffect, useContext } from "react";
-import { atom, useAtom } from "jotai";
-import { formatUnits, parseEther } from "@ethersproject/units";
-import { Contract, Provider, setMulticallAddress } from "ethers-multicall";
-import { Web3Context } from "../contexts/Web3Context";
+import {useEffect, useContext} from "react";
+import {atom, useAtom} from "jotai";
+import {formatUnits, parseEther} from "@ethersproject/units";
+import {Contract, Provider, setMulticallAddress} from "ethers-multicall";
+import {Web3Context} from "../contexts/Web3Context";
 import config from "../config/config";
 
 const poolTokenBalanceAtom = atom({});
 export default poolTokenBalanceAtom;
 
 export function usePoolTokenBalance(pool) {
-  const { ethersProvider, providerNetwork } = useContext(Web3Context);
+  const {ethersProvider, providerNetwork} = useContext(Web3Context);
   const [poolTokenBalance, setPoolTokenBalance] = useAtom(poolTokenBalanceAtom);
 
   useEffect(() => {
     if (!ethersProvider || !pool) return;
-    fetchPoolTokenBalance(
-      ethersProvider,
-      providerNetwork,
-      pool,
-      setPoolTokenBalance
-    );
+    fetchPoolTokenBalance(ethersProvider, providerNetwork, pool, setPoolTokenBalance);
   }, [ethersProvider, pool]);
 
   return [poolTokenBalance, setPoolTokenBalance];
 }
 
-export async function fetchPoolTokenBalance(
-  provider,
-  providerNetwork,
-  pool,
-  setPoolTokenBalance
-) {
+export async function fetchPoolTokenBalance(provider, providerNetwork, pool, setPoolTokenBalance) {
   if (provider && pool) {
     try {
       setMulticallAddress(providerNetwork.chainId, config.multicall);
@@ -45,10 +35,7 @@ export async function fetchPoolTokenBalance(
       const amountsRes = await ethcallProvider.all(calls);
       let amounts = {};
       for (let i = 0; i < amountsRes.length; i++) {
-        amounts[pool.supportTokens[i].symbol] = formatUnits(
-          amountsRes[i],
-          pool.supportTokens[i].decimals
-        );
+        amounts[pool.supportTokens[i].symbol] = formatUnits(amountsRes[i], pool.supportTokens[i].decimals);
       }
       setPoolTokenBalance(amounts);
     } catch (e) {
